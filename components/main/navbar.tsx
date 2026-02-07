b77cca7b367cb145358c868343791b703a7a110a
Aziel Randel Rabano <imazielr@gmail.com>
2026-02-07 13:25:37 +0800

    remove auth buttons in navbar

--- components/main/navbar.tsx
+++ components/main/navbar.tsx
@@ -5,17 +5,14 @@ import { hasEnvVars } from "@/lib/utils";
 import { Suspense } from "react";
 
 export default function navbar() {
+  if (!hasEnvVars) {
+    throw new Error("Supabase env vars missing");
+  }
+
   return (
     <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-      <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
+      <div className="w-full max-w-5xl flex justify-end items-center p-3 px-5 text-sm">
         <ThemeSwitcher />
-        {!hasEnvVars ? (
-          <EnvVarWarning />
-        ) : (
-          <Suspense>
-            <AuthButton />
-          </Suspense>
-        )}
       </div>
     </nav>
   );
