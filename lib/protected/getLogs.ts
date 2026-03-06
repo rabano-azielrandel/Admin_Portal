a5f1f52ac6bff5dfe2c48fe343f17c59fd9eca20
Aziel Randel Rabano <imazielr@gmail.com>
2026-03-06 18:02:25 +0800

    update getLogs to use supabase server

--- lib/protected/getLogs.ts
+++ lib/protected/getLogs.ts
@@ -1,16 +1,14 @@
-import { createClient } from "@/lib/supabase/client";
+import { createClient } from "@/lib/supabase/server";
 
 export async function getAuthLogs() {
-  const supabase = createClient();
+  const supabase = await createClient();
 
   const { data, error } = await supabase
     .from("weekly_auth_logs")
     .select("*")
     .order("created_at", { ascending: false });
 
-  if (error) {
-    throw new Error(error.message);
-  }
+  if (error) throw new Error(error.message);
 
   return data;
 }
