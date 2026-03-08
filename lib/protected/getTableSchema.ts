902c9dbe22315f1f53dcfe661a9d800d761c5a85
Aziel Randel Rabano <imazielr@gmail.com>
2026-03-08 10:32:31 +0800

    update getTableSchema BL

--- lib/protected/getTableSchema.ts
+++ lib/protected/getTableSchema.ts
@@ -1,15 +1,15 @@
 import { createClient } from "../supabase/server";
 
 export async function getTableSchema(tableName : string) {
-    const supabase = await createClient();
-    let table = tableName ? tableName : 'auth_logs';
-    
+    if (!tableName) throw new Error("tableName is required");
 
+    const supabase = await createClient();
+    
     const { data, error } = await supabase
     .from('information_schema.columns')
     .select('column_name, data_type')
     .eq('table_schema', 'public')
-    .eq('table_name', table);
+    .eq('table_name', tableName);
 
     if (error) throw new Error(error.message)
 
